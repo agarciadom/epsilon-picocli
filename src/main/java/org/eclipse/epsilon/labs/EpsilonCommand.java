f31b3c64b0374bddb6c38bfa04187c641919854b
Antonio García-Domínguez <a.garcia-dominguez@york.ac.uk>
2023-10-30 00:52:15 +0000

    Add EVL and generalise options

--- src/main/java/org/eclipse/epsilon/labs/EpsilonCommand.java
+++ src/main/java/org/eclipse/epsilon/labs/EpsilonCommand.java
@@ -2,6 +2,7 @@ package org.eclipse.epsilon.labs;
 
 import java.io.File;
 import java.net.URI;
+import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EParameter;
@@ -31,34 +32,38 @@ import org.eclipse.epsilon.eol.types.EolSequence;
 import org.eclipse.epsilon.eol.types.EolSet;
 import org.eclipse.epsilon.eol.types.concurrent.EolConcurrentBag;
 import org.eclipse.epsilon.eol.types.concurrent.EolConcurrentSet;
+import org.eclipse.epsilon.evl.EvlModule;
+import org.eclipse.epsilon.evl.parse.EvlParser;
 import org.eclipse.epsilon.flexmi.FlexmiResourceFactory;
 
 import io.micronaut.configuration.picocli.PicocliRunner;
 import io.micronaut.core.annotation.ReflectionConfig;
 import io.micronaut.core.annotation.TypeHint;
 import io.micronaut.core.annotation.TypeHint.AccessType;
+import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 /*
- * Note: a good part of this could be perhaps automated with a GraalVM Feature:
- *
- * https://www.graalvm.org/latest/reference-manual/native-image/dynamic-features/Reflection/#configuration-with-features
+ * Ecore types that are used reflectively.
  */
 @TypeHint(value = {
-    EParameter[].class,
-    EStringToStringMapEntryImpl[].class,
-    ETypeParameter[].class
+        EParameter[].class,
+        EStringToStringMapEntryImpl[].class,
+        ETypeParameter[].class
 })
-@ReflectionConfig(
-    type = EolParser.class,
-    accessType = {
+/* Epsilon parsers */
+@ReflectionConfig(type = EolParser.class, accessType = {
         AccessType.ALL_DECLARED_FIELDS,
         AccessType.ALL_DECLARED_METHODS,
         AccessType.ALL_DECLARED_CONSTRUCTORS
-    }
-)
+})
+@ReflectionConfig(type = EvlParser.class, accessType = {
+        AccessType.ALL_DECLARED_FIELDS,
+        AccessType.ALL_DECLARED_METHODS,
+        AccessType.ALL_DECLARED_CONSTRUCTORS
+})
 /* EMF models */
 @ReflectionConfig(type = EmfModel.class, accessType = AccessType.ALL_PUBLIC_METHODS)
 @ReflectionConfig(type = EolModelElementType.class, accessType = AccessType.ALL_PUBLIC_METHODS)
@@ -83,34 +88,51 @@ import picocli.CommandLine.Parameters;
 @ReflectionConfig(type = EolSequence.class, accessType = AccessType.ALL_PUBLIC_METHODS)
 @ReflectionConfig(type = EolSet.class, accessType = AccessType.ALL_PUBLIC_METHODS)
 /* Picocli command */
-@Command(name = "epsilon", description = "Runs an Epsilon script", mixinStandardHelpOptions = true)
+@Command(name = "epsilon", description = "Runs an Epsilon Object Language / Epsilon Validation Language script.", mixinStandardHelpOptions = true)
 public class EpsilonCommand implements Runnable {
 
-    @Option(names = { "-v", "--verbose" }, description = "...")
-    boolean verbose;
+    @Parameters(index = "0", description = "Path to the EOL/EVL script")
+    private File scriptPath;
 
-    @Parameters(index = "0")
-    private File eolPath;
+    @ArgGroup(exclusive = false, multiplicity = "0..*")
+    List<ModelOptions> models;
 
-    @Parameters(index = "1")
-    private File metamodelPath;
-
-    @Parameters(index = "2")
-    private File modelPath;
+    static class ModelOptions {
+        @Option(names = { "-n",
+                "--model-name" }, required = true, defaultValue = "Model", description = "Name of the model (defaults to 'Model')")
+        String name;
+        @Option(names = { "-f",
+                "--model-file" }, required = true, description = "Path to the model file (in .xmi or .flexmi format)")
+        File modelFile;
+        @Option(names = { "-m",
+                "--metamodel-file" }, description = "Path to a metamodel file (in .ecore or .emf format)")
+        List<File> metamodelFiles;
+        @Option(names = { "-r", "--read-on-load" }, description = "If used, will read the model from disk upon load (default is not to read)")
+        boolean readOnLoad;
+        @Option(names = { "-s",
+                "--store-on-disposal" }, description = "If used, will store the model to disk upon disposal (default is not to store)")
+        boolean storeOnDisposal;
+    }
 
     public static void main(String[] args) throws Exception {
         PicocliRunner.run(EpsilonCommand.class, args);
     }
 
     public void run() {
+        EolModule module = null;
+
         try {
             // Register the Flexmi and Emfatic parsers with EMF
             Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("flexmi", new FlexmiResourceFactory());
             Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("emf", new EmfaticResourceFactory());
 
             // Parse the EOL program
-            EolModule module = new EolModule();
-            URI fileURI = eolPath.toURI();
+            if ("evl".equals(scriptPath.getName().toLowerCase())) {
+                module = new EvlModule();
+            } else {
+                module = new EolModule();
+            }
+            URI fileURI = scriptPath.toURI();
             module.parse(fileURI);
             if (!module.getParseProblems().isEmpty()) {
                 for (ParseProblem problem : module.getParseProblems()) {
@@ -119,25 +141,29 @@ public class EpsilonCommand implements Runnable {
                 System.exit(1);
             }
 
-            // Load the model from model.flexmi using metamodel.emf as its metamodel
-            EmfModel model = new EmfModel();
-            model.setName("M");
-            model.setModelFile(modelPath.getPath());
-            model.setMetamodelFile(metamodelPath.getPath());
-            model.setReadOnLoad(true);
-            model.setStoredOnDisposal(false);
-            model.load();
+            if (models != null) {
+                for (ModelOptions model : models) {
+                    EmfModel m = new EmfModel();
+                    m.setName(model.name);
+                    m.setModelFile(model.modelFile.getPath());
+                    m.setMetamodelFiles(model.metamodelFiles.stream().map(f -> f.getPath()).toList());
+                    m.setReadOnLoad(model.readOnLoad);
+                    m.setStoredOnDisposal(model.storeOnDisposal);
+                    m.load();
 
-            // Make the model available to the program
-            module.getContext().getModelRepository().addModel(model);
+                    module.getContext().getModelRepository().addModel(m);
+                }
+            }
 
             // Execute the EOL program
             module.execute();
-
-            // Dispose of the model
-            module.getContext().getModelRepository().dispose();
         } catch (Exception ex) {
             ex.printStackTrace();
+        } finally {
+            if (module != null) {
+                // Dispose of the model
+                module.getContext().getModelRepository().dispose();
+            }
         }
     }
 }
